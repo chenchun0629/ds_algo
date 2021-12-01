cebfcce115032d55b0ba273e982b7f92a05f1205
chenchun <pangou@live.cn>
2021-12-01 14:19:13 +0800

    feat: bounded load consistent hashing

--- consistent_hashing/consistent.go
+++ consistent_hashing/consistent.go
@@ -4,8 +4,10 @@ import (
 	"errors"
 	"fmt"
 	"hash/crc32"
+	"math"
 	"sort"
 	"sync"
+	"sync/atomic"
 )
 
 type uints []uint32
@@ -16,22 +18,136 @@ func (x uints) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
 
 var ErrEmptyNodes = errors.New("empty nodes")
 
+type options struct {
+	replicas int // 副本/虚拟节点数量
+
+	loadFactor float64 // 负载因子
+}
+
+func (o *options) apply(fn ...SetOptionsFunc) {
+	for _, v := range fn {
+		v(o)
+	}
+}
+
+func newOptions() *options {
+	return &options{
+		replicas:   20,
+		loadFactor: 1.25,
+	}
+}
+
+type SetOptionsFunc func(*options)
+
+func SetOptionsReplicas(replicas int) SetOptionsFunc {
+	return func(o *options) {
+		o.replicas = replicas
+	}
+}
+func SetOptionsLoadFactor(loadFactor float64) SetOptionsFunc {
+	return func(o *options) {
+		o.loadFactor = loadFactor
+	}
+}
+
+type Node struct {
+	Name string
+	Load int64
+}
+
 type Consistent struct {
-	replicas int
+	options *options
 
 	circle       map[uint32]string
-	nodes        map[string]bool
+	nodes        map[string]*Node
 	sortedHashes uints
+
+	totalLoad int64
 
 	sync.RWMutex
 }
 
-func NewConsistent(replicas int) *Consistent {
+func NewConsistent(fn ...SetOptionsFunc) *Consistent {
+	var o = newOptions()
+	o.apply(fn...)
+
 	return &Consistent{
-		replicas: replicas,
-		circle:   make(map[uint32]string),
-		nodes:    make(map[string]bool),
+		options:   o,
+		circle:    make(map[uint32]string),
+		nodes:     make(map[string]*Node),
+		totalLoad: 0,
 	}
+}
+
+func (c *Consistent) Incr(node string) {
+	c.Lock()
+	defer c.Unlock()
+
+	atomic.AddInt64(&c.nodes[node].Load, 1)
+	atomic.AddInt64(&c.totalLoad, 1)
+}
+
+func (c *Consistent) Done(node string) {
+	c.Lock()
+	defer c.Unlock()
+
+	if _, ok := c.nodes[node]; !ok {
+		return
+	}
+	atomic.AddInt64(&c.nodes[node].Load, -1)
+	atomic.AddInt64(&c.totalLoad, -1)
+}
+
+func (c *Consistent) GetLeast(key string) (string, error) {
+	c.RLock()
+	defer c.RUnlock()
+
+	if len(c.nodes) == 0 {
+		return "", ErrEmptyNodes
+	}
+
+	var (
+		h   = c.hashKey(key)
+		idx = c.search(h)
+		i   = idx
+	)
+
+	for {
+		var node = c.circle[c.sortedHashes[i]]
+		if c.loadNotOver(node) {
+			return node, nil
+		}
+		i++
+		if i >= len(c.circle) {
+			i = 0
+		}
+
+		if i == idx {
+			break
+		}
+	}
+
+	return c.circle[c.sortedHashes[idx]], nil
+}
+
+func (c *Consistent) loadNotOver(node string) bool {
+	if c.totalLoad < 0 {
+		c.totalLoad = 0
+	}
+
+	var avgLoad = float64((c.totalLoad + 1) / int64(len(c.nodes)))
+	avgLoad = math.Ceil(avgLoad * c.options.loadFactor)
+
+	var n, has = c.nodes[node]
+	if !has {
+		panic(fmt.Sprintf("given node(%s) not in nodes", n.Name))
+	}
+
+	if float64(n.Load) <= avgLoad {
+		return true
+	}
+
+	return false
 }
 
 func (c *Consistent) Get(key string) (string, error) {
@@ -61,26 +177,29 @@ func (c *Consistent) search(key uint32) (i int) {
 	return
 }
 
-func (c *Consistent) Add(nodeName string) {
+func (c *Consistent) Add(key string) {
 	c.Lock()
 	defer c.Unlock()
 
-	for i := 0; i < c.replicas; i++ {
-		c.circle[c.hashKey(c.genNodeKey(nodeName, i))] = nodeName
+	for i := 0; i < c.options.replicas; i++ {
+		c.circle[c.hashKey(c.genNodeKey(key, i))] = key
 	}
 
 	c.updateSortedHashes()
-	c.nodes[nodeName] = true
+	c.nodes[key] = &Node{
+		Name: key,
+		Load: 0,
+	}
 }
 
-func (c *Consistent) Remove(nodeName string) {
+func (c *Consistent) Remove(key string) {
 	c.Lock()
 	defer c.Unlock()
 
-	for i := 0; i < c.replicas; i++ {
-		delete(c.circle, c.hashKey(c.genNodeKey(nodeName, i)))
+	for i := 0; i < c.options.replicas; i++ {
+		delete(c.circle, c.hashKey(c.genNodeKey(key, i)))
 	}
-	delete(c.nodes, nodeName)
+	delete(c.nodes, key)
 	c.updateSortedHashes()
 }
 
@@ -95,7 +214,7 @@ func (c *Consistent) genNodeKey(node string, index int) string {
 func (c *Consistent) updateSortedHashes() {
 	hashes := c.sortedHashes[:0]
 	//reallocate if we're holding on to too much (1/4th)
-	if cap(c.sortedHashes)/(c.replicas*4) > len(c.circle) {
+	if cap(c.sortedHashes)/(c.options.replicas*4) > len(c.circle) {
 		hashes = nil
 	}
 	for k := range c.circle {
--- consistent_hashing/consistent_test.go
+++ consistent_hashing/consistent_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 )
 
-func TestConsistent(t *testing.T) {
-	var c = NewConsistent(20)
+func TestConsistent_Get(t *testing.T) {
+	var c = NewConsistent(SetOptionsReplicas(20))
 	c.Add("127.0.0.1:8080")
 	c.Add("127.0.0.1:8081")
 	c.Add("127.0.0.1:8082")
@@ -26,6 +26,34 @@ func TestConsistent(t *testing.T) {
 			if err == nil {
 				m[node]++
 			}
+		}
+		fmt.Println(m)
+	}
+
+}
+
+func TestConsistent_GetLeast(t *testing.T) {
+	var c = NewConsistent(SetOptionsReplicas(20), SetOptionsLoadFactor(1.25))
+	c.Add("127.0.0.1:8080")
+	c.Add("127.0.0.1:8081")
+	c.Add("127.0.0.1:8082")
+	c.Add("127.0.0.1:8083")
+
+	var (
+		table = []string{
+			"lock:order:id:%d",
+		}
+	)
+
+	for _, v := range table {
+
+		var m = make(map[string]int)
+		for i := 0; i < 1000000; i++ {
+			var node, err = c.GetLeast(fmt.Sprintf(v, 1))
+			if err == nil {
+				m[node]++
+			}
+			c.Incr(node)
 		}
 		fmt.Println(m)
 	}
